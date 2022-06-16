48470725e9bf234375074b553ccaeabbcb3c772c
nachorvw <nachorewe@gmail.com>
2022-06-16 18:08:34 -0300

    Creada funcion Dolar_Blue en js, combinado al html y extendida la funcion del api para el dolar blue

--- dolarAPI.js
+++ dolarAPI.js
@@ -3,7 +3,7 @@ let DOventa = document.getElementById("venta");
 let DOvariacion = document.getElementById("varia");
 let fecha = document.getElementById("fecha");
 let hoy = new Date();
-
+const api_url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
 function traer() {
   fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
     .then((res) => res.json())
@@ -17,3 +17,23 @@ function traer() {
 }
 
 traer();
+async function Dolar_Blue() {
+  const Respuesta = await fetch(api_url);
+  const Data = await Respuesta.json();
+  document.getElementById("Compra_Blue").textContent = Data[1].casa.compra;
+  document.getElementById("Venta_Blue").textContent = Data[1].casa.venta;
+  document.getElementById("Variacion_Blue").textContent =
+    Data[1].casa.variacion;
+  document.getElementById("Fecha").textContent = hoy;
+}
+Dolar_Blue();
+async function Mostrar_Consola() {
+  const Respuesta = await fetch(api_url);
+  const Data = await Respuesta.json();
+
+  console.log(Data[1]);
+  console.log(Data[1].casa.compra);
+  console.log(Data[1].casa.venta);
+  console.log(Data[1].casa.variacion);
+}
+Mostrar_Consola();
