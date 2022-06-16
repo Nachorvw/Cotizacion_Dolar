a64ddd2a44176bee005cbdc2d6f5de17b5958e5d
German Himmel <ghimmel98@gmail.com>
2022-06-16 09:40:26 -0300

    terminando de conectar API

--- dolarAPI.js
+++ dolarAPI.js
@@ -1,5 +1,9 @@
 let compra = document.querySelector("#compra");
 let venta = document.querySelector("#venta");
+let variacion = document.querySelector("#varia");
+let fecha = document.querySelector("#fecha");
+let hoy = new Date();
+
 function traer() {
   fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
     .then((res) => res.json())
@@ -7,6 +11,8 @@ function traer() {
       console.log(date[0].casa.compra);
       compra.innerHTML = `<p> ${date[0].casa.compra}</p>`;
       venta.innerHTML = `<p> ${date[0].casa.venta}</p>`;
+      variacion.innerHTML = `<p>Variacion : ${date[0].casa.variacion}</p>`;
+      fecha.innerHTML = `<p>${hoy.toDateString()}</p>`;
     });
 }
 
